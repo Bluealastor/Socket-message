a152447857e5ba3651011d25f9f17b865f3dd46b
Christian <chrise79456@gmail.com>
2024-07-18 16:23:54 +0200

    first commit

---
+++ src/config/config.ts
@@ -0,0 +1,8 @@
+import { config } from 'dotenv';
+
+config();
+
+export const BASE_URL = process.env.BASE_URL;
+export const PORT = process.env.PORT;
+export const PORT3 = process.env.PORT3;
+
---
+++ src/index.ts
@@ -0,0 +1,70 @@
+import express, { Response, Request } from "express";
+
+import { createClient } from "@vercel/postgres";
+import { BASE_URL, PORT, PORT3 } from "./config/config";
+import { Server } from "socket.io";
+import { createServer } from "http";
+import cors from "cors"
+
+const app = express()
+const server =  createServer(app)
+const io = new Server(server,{
+    cors:{
+        origin:"*",
+        methods:["GET", "POST"]
+    }
+})
+
+
+const client = createClient({
+    connectionString: BASE_URL
+})
+
+client.connect()
+app.use(cors({
+    origin:"*",
+    methods:["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
+    allowedHeaders: ["Content-Type", "Authorization"]
+}))
+
+io.on("connection",(socket) => {
+    console.log("bella fra")
+    socket.on("message-sent", (message)=>{
+        client.query("INSERT INTO messages (content) VALUES($1)",[message],
+            (error)=>{
+                if(!error)io.emit("message-received",message)
+            }
+        )
+        console.log(message)
+        io.emit("message-recived", message)
+    })
+    socket.on("disconnect", ()=>{
+        console.log("server disconnect")
+    })
+})
+
+app.get("/", (req: Request, res: Response)=>{
+    res.send("Hello World")
+})
+
+app.get("/api/messages", (requirement: Request,response:Response)=>{
+   client.query("SELECT * FROM messages",(err,res)=>{
+        err ? response.status(400).json({err}) : response.status(200).json(res.rows)
+    })
+})
+
+app.post("/api/messages", async (requirement: Request,response:Response)=> {
+client.query("INSERT INTO messages (content) VALUES ($1)",[requirement.body.content],
+    (err,res)=>{ err ? response.status(500).json({message: err}) :
+     response.status(200).json({message: "Update Data"})})
+
+})
+
+
+server.listen(PORT3),()=>{
+    console.log("Server is running in http://localhost:"+PORT3)
+}
+
+app.listen(PORT,()=>{
+    console.log("APP is Running in http://localhost:"+PORT)
+})
