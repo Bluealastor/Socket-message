514301bf3670feeed09fb33843480e82fc1ea120
Christian <chrise79456@gmail.com>
2024-07-19 16:51:15 +0200

    changed files deploy

--- src/config/config.ts
+++
@@ -1,8 +0,0 @@
-import { config } from 'dotenv';
-
-config();
-
-export const BASE_URL = process.env.BASE_URL;
-export const PORT = process.env.PORT8080;
-export const PORT3 = process.env.PORT3000;
-
--- src/server.ts
+++ src/server.ts
@@ -1,67 +1,79 @@
-import express, { Response, Request } from "express";
-
+// #region ::: IMPORTS :::
 import { createClient } from "@vercel/postgres";
-import { BASE_URL, PORT, PORT3 } from "./config/config";
+import express, { Request, Response } from "express";
+import { config } from "dotenv";
 import { Server } from "socket.io";
 import { createServer } from "http";
-import cors from "cors"
+import cors from "cors";
 import path from "path";
 
-const app = express()
-const server =  createServer(app)
-const io = new Server(server,{
-    cors:{
-        origin:"*",
-        methods:["GET", "POST"]
-    }
-})
+config();
 
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+const server = createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
 
 const client = createClient({
-    connectionString: BASE_URL
-})
+  connectionString: process.env.BASE_URL,
+});
 
-client.connect()
-app.use(cors({
-    origin:"*",
-    methods:["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
-    allowedHeaders: ["Content-Type", "Authorization"]
-}))
+client.connect();
 
-io.on("connection",(socket) => {
-    console.log("bella fra")
-    socket.on("message-sent", (message)=>{
-        client.query("INSERT INTO messages (content) VALUES($1)",[message],
-            (error)=>{
-                if(!error)io.emit("message-received",message)
-            }
-        )
-        console.log(message)
-        io.emit("message-recived", message)
-    })
-    socket.on("disconnect", ()=>{
-        console.log("server disconnect")
-    })
-})
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+  express.json()
+);
 
-app.get("/", (req: Request, res: Response)=>{
-    res.sendFile(path.join(__dirname, "index.html"))
-})
+io.on("connection", (socket) => {
+  socket.on("message-sent", (message) => {
+    client.query(
+      `INSERT INTO messages (content) VALUES ($1)`,
+      [message],
+      (error) => {
+        if (!error) io.emit("message-received", message);
+      }
+    );
+  });
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
+  });
+});
 
-app.get("/api/messages", (requirement: Request,response:Response)=>{
-   client.query("SELECT * FROM messages",(err,res)=>{
-        err ? response.status(400).json({err}) : response.status(200).json(res.rows)
-    })
-})
+app.get("/", (req: Request, res: Response) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
 
-app.post("/api/messages", async (requirement: Request,response:Response)=> {
-client.query("INSERT INTO messages (content) VALUES ($1)",[requirement.body.content],
-    (err,res)=>{ err ? response.status(500).json({message: err}) :
-     response.status(200).json({message: "Update Data"})})
+app.get("/api/messages", (req: Request, res: Response) => {
+  client.query("SELECT * FROM messages", (error, response) => {
+    if (error) res.status(500).json({ error });
+    else res.status(200).json(response.rows);
+  });
+});
 
-})
+app.post("/api/messages", (req: Request, res: Response) => {
+  const { content } = req.body;
+  client.query(
+    `INSERT INTO messages (content) VALUES ($1)`,
+    [content],
+    (error) => {
+      if (error) res.status(500).json({ error });
+      else res.status(200).json({ message: "Message created successfully" });
+    }
+  );
+});
 
-
-server.listen(PORT3),()=>{
-    console.log("Server is running in http://localhost:"+PORT3)
-}
+server.listen(PORT, () => {
+  console.log(`Server API is running http://localhost:${PORT}`);
+});
